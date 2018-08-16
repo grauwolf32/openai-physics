1415a53b5b3dbff0d26662b94c0c67f67fbe66c7
grauwolf32 <grauwolf86@gmail.com>
2018-08-16 21:52:14 +0300

    some corrections

--- physics.py
+++ physics.py
@@ -43,10 +43,10 @@ r2 = A[1,:] + B[1,:]
 r3 = A[2,:] + B[2,:]
 r4 = A[3,:] + B[3,:]
 
-J = J_ball + np.linalg.outer(r1,r1) * dot_masses[0] \
-           + np.linalg.outer(r2,r2) * dot_masses[1] \ 
-           + np.linalg.outer(r3,r3) * dot_masses[2] \
-           + np.linalg.outer(r4,r4) * dot_masses[3] 
+J = J_ball + (np.dot(r1,r1)*np.eye(3) - np.linalg.outer(r1,r1))* dot_masses[0] \ 
+           + (np.dot(r2,r2)*np.eye(3) - np.linalg.outer(r2,r2))* dot_masses[1] \ 
+           + (np.dot(r3,r3)*np.eye(3) - np.linalg.outer(r3,r3))* dot_masses[2] \
+           + (np.dot(r4,r4)*np.eye(3) - np.linalg.outer(r4,r4))* dot_masses[3]
 
 w1 = A[0,:] / np.linalg.norm(A[0,:]) * w_start[0]
 w2 = A[1,:] / np.linalg.norm(A[1,:]) * w_start[1]
