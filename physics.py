2dc83c5f20ced101c198a6fb3e9a06255edf74e8
grauwolf32 <grauwolf86@gmail.com>
2018-08-18 01:44:05 +0300

    v.0.5

--- physics.py
+++ physics.py
@@ -1,10 +1,10 @@
 import numpy as np 
-from aux import rotate_vec
+from aux import rotate_vec, get_J_inertion
 
 tetra_len = 1.0
 r_ball = tetra_len * np.sqrt(3.0/8.0)
+Massess = (m_ball + np.sum(dot_masses))
     
-relative_system = np.eye(3)
 absolute_system = np.eye(3)
 
 position = start_position                                 # position of center of the ball
@@ -54,51 +54,31 @@ b4 = np.dot(rotate_vec(A[3,:], phi_start[3]), b4)
 
 B = np.array([b1, b2, b3, b4])
 R = A + B + np.array([gamma]*4)
-
-J = J_ball + (np.dot(R[0],R[0])*np.eye(3) - np.linalg.outer(R[0],R[0])) * dot_masses[0] \ 
-           + (np.dot(R[1],R[1])*np.eye(3) - np.linalg.outer(R[1],R[1])) * dot_masses[1] \ 
-           + (np.dot(R[2],R[2])*np.eye(3) - np.linalg.outer(R[2],R[2])) * dot_masses[2] \
-           + (np.dot(R[3],R[3])*np.eye(3) - np.linalg.outer(R[3],R[3])) * dot_masses[3]
+J = J_ball + get_J_inertion(R, dot_masses)
 
 W = np.dot(U, np.diag(w_abs))
 E = np.dot(U, np.diag(e_abs))
 
-dr1 = np.cross(Omega, A[0] + gamma) + np.cross(Omega + W[0,:], B[0,:]) 
-dr2 = np.cross(Omega, A[1] + gamma) + np.cross(Omega + W[1,:], B[1,:]) 
-dr3 = np.cross(Omega, A[2] + gamma) + np.cross(Omega + W[2,:], B[2,:]) 
-dr4 = np.cross(Omega, A[3] + gamma) + np.cross(Omega + W[3,:], B[3,:]) 
-
-dJdt = (np.diag([2.0*np.dot(r1,dr1)]*3) - np.linalg.outer(dr1,r1) - np.linalg.outer(r1,dr1)) * dot_masses[0] +\
-       (np.diag([2.0*np.dot(r2,dr2)]*3) - np.linalg.outer(dr2,r2) - np.linalg.outer(r2,dr2)) * dot_masses[1] +\ 
-       (np.diag([2.0*np.dot(r3,dr3)]*3) - np.linalg.outer(dr3,r3) - np.linalg.outer(r3,dr3)) * dot_masses[2] +\
-       (np.diag([2.0*np.dot(r4,dr4)]*3) - np.linalg.outer(dr4,r4) - np.linalg.outer(r4,dr4)) * dot_masses[3] 
+dR = get_dR(A, B, W, Omega, gamma)
+dJdt = get_dJdt(R, dR, dot_masses)
 
 Omega = Omega_start
-
-f1 = (np.cross(E[0,:], B[0,:]) + np.cross(W[0,:],np.cross(W[0,:], B[0,:])) + g_abs + np.cross(Omega, np.cross(W[0,:], B[0,:]))) * dot_masses[0]
-f2 = (np.cross(E[1,:], B[1,:]) + np.cross(W[1,:],np.cross(W[1,:], B[1,:])) + g_abs + np.cross(Omega, np.cross(W[1,:], B[1,:]))) * dot_masses[1]
-f3 = (np.cross(E[2,:], B[2,:]) + np.cross(W[2,:],np.cross(W[2,:], B[2,:])) + g_abs + np.cross(Omega, np.cross(W[2,:], B[2,:]))) * dot_masses[2]
-f4 = (np.cross(E[3,:], B[3,:]) + np.cross(W[3,:],np.cross(W[3,:], B[3,:])) + g_abs + np.cross(Omega, np.cross(W[3,:], B[3,:]))) * dot_masses[3]
-
+F = get_F(B, E, W, Omega, g_abs, dot_masses)
 f_ball = g_abs * m_ball
-F = np.array([f1, f2, f3, f4])
 
-Mo = np.cross(R[0], F[0]) + \
-     np.cross(R[1], F[1]) + \
-     np.cross(R[2], F[2]) + \
-     np.cross(R[3], F[3]) 
-     # Gravity force of the ball made no moment aganist tangent point
+Mo =  np.sum([np.cross(R[i], F[i]) for i in xrange(0, R.shape[0])]) 
+# Gravity force of the ball made no moment aganist tangent point
 
 F_all = np.sum(F, axis=0) + f_ball
 React = -np.dot(F_all, absolute_system[3,:])*absolute_system[3,:]
 F_act = F_all + React # Must be parallel to the plain
 
-dOmegadt = np.linalg.inv(J)*(Mo + np.dot(dJdt,Omega))
+dOmegadt = np.linalg.inv(J)*(Mo + np.dot(dJdt, Omega))
 
 while t < T:
     Omega = Omega + dOmegadt*dt
     position = position + velocity*dt
-    velocity = velocity + F_act * dt / (m_ball + np.sum(dot_masses))
+    velocity = velocity + F_act * dt / Massess
 
     tangent_point = np.array([position[0], position[1], 0.0]) # point in common of the sphere and the plain
     gamma = position - tangent_point
@@ -117,34 +97,18 @@ while t < T:
     B[3] = B[3] + np.cross(Omega + W[3], B[3])*dt
 
     R = A + B + np.array([gamma]*4)
+    J = J_ball + get_J_inertion(R, dot_masses)
 
     w_abs  = w_abs + e_abs * dt
     W = np.dot(U, np.diag(w_abs))
     E = np.dot(U, np.diag(e_abs))
     e_abs = controller() # input from controller
 
-    dr1 = np.cross(Omega, A[0] + gamma) + np.cross(Omega + W[0,:], B[0,:]) 
-    dr2 = np.cross(Omega, A[1] + gamma) + np.cross(Omega + W[1,:], B[1,:]) 
-    dr3 = np.cross(Omega, A[2] + gamma) + np.cross(Omega + W[2,:], B[2,:]) 
-    dr4 = np.cross(Omega, A[3] + gamma) + np.cross(Omega + W[3,:], B[3,:]) 
+    dR = get_dR(A, B, W, Omega, gamma)
+    dJdt = get_dJdt(R, dR, dot_masses)
 
-    dJdt = (np.diag([2.0*np.dot(r1,dr1)]*3) - np.linalg.outer(dr1,r1) - np.linalg.outer(r1,dr1)) * dot_masses[0] +\
-           (np.diag([2.0*np.dot(r2,dr2)]*3) - np.linalg.outer(dr2,r2) - np.linalg.outer(r2,dr2)) * dot_masses[1] +\ 
-           (np.diag([2.0*np.dot(r3,dr3)]*3) - np.linalg.outer(dr3,r3) - np.linalg.outer(r3,dr3)) * dot_masses[2] +\
-           (np.diag([2.0*np.dot(r4,dr4)]*3) - np.linalg.outer(dr4,r4) - np.linalg.outer(r4,dr4)) * dot_masses[3] 
-
-    f1 = (np.cross(E[0,:], B[0,:]) + np.cross(W[0,:],np.cross(W[0,:], B[0,:])) + g_abs + np.cross(Omega, np.cross(W[0,:], B[0,:]))) * dot_masses[0]
-    f2 = (np.cross(E[1,:], B[1,:]) + np.cross(W[1,:],np.cross(W[1,:], B[1,:])) + g_abs + np.cross(Omega, np.cross(W[1,:], B[1,:]))) * dot_masses[1]
-    f3 = (np.cross(E[2,:], B[2,:]) + np.cross(W[2,:],np.cross(W[2,:], B[2,:])) + g_abs + np.cross(Omega, np.cross(W[2,:], B[2,:]))) * dot_masses[2]
-    f4 = (np.cross(E[3,:], B[3,:]) + np.cross(W[3,:],np.cross(W[3,:], B[3,:])) + g_abs + np.cross(Omega, np.cross(W[3,:], B[3,:]))) * dot_masses[3]
-
-    f_ball = g_abs * m_ball
-    F = np.array([f1, f2, f3, f4])
-
-    Mo = np.cross(R[0], F[0]) + \
-         np.cross(R[1], F[1]) + \
-         np.cross(R[2], F[2]) + \
-         np.cross(R[3], F[3]) 
+    F = get_F(B, E, W, Omega, g_abs, dot_masses)
+    Mo =  np.sum([np.cross(R[i], F[i]) for i in xrange(0, R.shape[0])])
 
     F_all = np.sum(F, axis=0) + f_ball
     React = -np.dot(F_all, absolute_system[3,:])*absolute_system[3,:]
