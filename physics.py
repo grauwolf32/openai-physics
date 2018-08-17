1ba99b88ea838b9cbdf179196f5a7c102e8fd36b
grauwolf32 <grauwolf86@gmail.com>
2018-08-17 15:08:27 +0300

    refactored

--- physics.py
+++ physics.py
@@ -15,18 +15,28 @@ tangent_point = np.array([position[0], position[1], 0.0]) # point in common of t
 gamma = ball_center - tangent_point # vector from tangent_point to the center of the sphere
 
 g_abs = absolute_system[2, :] * 9.8 # gravity force field vector
+w_abs = w_start # absolute values of angular velocity and
+e_abs = e_start # angular acceleration
 
 J_ball = np.eye(3)*( 2.0/5.0 * m_ball**2)
 J_ball[0][0] += m_ball*r_ball**2 / 4.0
 J_ball[1][1] += m_ball*r_ball**2 / 4.0
 
-alpha = np.arccos(-1.0/3.0)
-a1 = relative_system[2,:] / np.linalg.norm(relative_system[2,:]) * np.sqrt(1.0/8.0)
-a2 = np.dot(rotate_vec(relative_system[0,:], alpha), a1)
-a3 = np.dot(rotate_vec(relative_system[2,:], 2.0/3.0*np.pi), a2)
-a4 = np.dot(rotate_vec(relative_system[2,:], 2.0/3.0*np.pi), a3)
+alpha  = np.arccos(-1.0/3.0)
+length = np.sqrt(1.0/8.0)
 
-A = np.array([a1, a2, a3, a4])
+u1 = relative_system[2,:] / np.linalg.norm(relative_system[2,:]) 
+u2 = np.dot(rotate_vec(relative_system[0,:], alpha), a1)
+u3 = np.dot(rotate_vec(relative_system[2,:], 2.0/3.0*np.pi), a2)
+u4 = np.dot(rotate_vec(relative_system[2,:], 2.0/3.0*np.pi), a3)
+
+u1 = u1 / np.linalg.norm(u1)
+u2 = u2 / np.linalg.norm(u2)
+u3 = u3 / np.linalg.norm(u3)
+u4 = u4 / np.linalg.norm(u4)
+
+U = np.array([u1, u2, u3, u4])
+A = np.dot(U, np.diag([np.sqrt(1.0/8.0)]*4))
 
 b1 = np.cross(A[0,:], A[1,:])
 b2 = np.cross(A[1,:], A[2,:])
@@ -45,29 +55,14 @@ b4 = np.dot(rotate_vec(A[3,:], phi_start[3]), b4)
 
 B = np.array([b1, b2, b3, b4])
 
-r1 = A[0,:] + B[0,:]
-r2 = A[1,:] + B[1,:]
-r3 = A[2,:] + B[2,:]
-r4 = A[3,:] + B[3,:]
-
-R = np.array([r1, r2, r3, r4])
-
+R = A + B
 J = J_ball + (np.dot(r1,r1)*np.eye(3) - np.linalg.outer(r1,r1)) * dot_masses[0] \ 
            + (np.dot(r2,r2)*np.eye(3) - np.linalg.outer(r2,r2)) * dot_masses[1] \ 
            + (np.dot(r3,r3)*np.eye(3) - np.linalg.outer(r3,r3)) * dot_masses[2] \
            + (np.dot(r4,r4)*np.eye(3) - np.linalg.outer(r4,r4)) * dot_masses[3]
 
-w1 = A[0,:] / np.linalg.norm(A[0,:]) * w_start[0]
-w2 = A[1,:] / np.linalg.norm(A[1,:]) * w_start[1]
-w3 = A[2,:] / np.linalg.norm(A[2,:]) * w_start[2]
-w4 = A[3,:] / np.linalg.norm(A[3,:]) * w_start[3]
-W = np.array([w1, w2, w3, w4])
-
-e1 = A[0,:] / np.linalg.norm(A[0,:]) * e_start[0]
-e2 = A[1,:] / np.linalg.norm(A[1,:]) * e_start[1]
-e3 = A[2,:] / np.linalg.norm(A[2,:]) * e_start[2]
-e4 = A[3,:] / np.linalg.norm(A[3,:]) * e_start[3]
-E = np.array([e1, e2, e3, e4])
+W = np.dot(U, np.diag(w_abs))
+E = np.dot(U, np.diag(e_abs))
 
 dr1 = np.cross(A[0] + gamma, Omega) + np.cross(B[0,:], W[0,:]) # Give right formulas
 dr2 = np.cross(A[1] + gamma, Omega) + np.cross(B[1,:], W[1,:]) #
@@ -78,12 +73,6 @@ dJdt = (np.linalg.outer(dr1,r1) + np.linalg.outer(r1,dr1))* dot_masses[0] +\
        (np.linalg.outer(dr2,r2) + np.linalg.outer(r2,dr2))* dot_masses[1] +\ 
        (np.linalg.outer(dr3,r3) + np.linalg.outer(r3,dr3))* dot_masses[2] +\
        (np.linalg.outer(dr4,r4) + np.linalg.outer(r4,dr4))* dot_masses[3] 
-
-e1 = A[0,:] / np.linalg.norm(A[0,:]) * e_start[0]
-e2 = A[1,:] / np.linalg.norm(A[1,:]) * e_start[1]
-e3 = A[2,:] / np.linalg.norm(A[2,:]) * e_start[2]
-e4 = A[3,:] / np.linalg.norm(A[3,:]) * e_start[3]
-E = np.array([e1, e2, e3, e4])
 
 Omega = Omega_start
 
@@ -98,7 +87,8 @@ F = np.array([f1, f2, f3, f4])
 Mo = np.cross(R[0]-tangent_point, F[0]) + \
      np.cross(R[1]-tangent_point, F[1]) + \
      np.cross(R[2]-tangent_point, F[2]) + \
-     np.cross(R[3]-tangent_point, F[3]) # Gravity force of the ball made no moment aganist tangent point
+     np.cross(R[3]-tangent_point, F[3]) 
+     # Gravity force of the ball made no moment aganist tangent point
 
 F_all = f1 + f2 + f3 + f4 + f_ball
 React = -np.dot(F_all, absolute_system[3,:])*absolute_system[3,:]
@@ -110,5 +100,19 @@ Omega = Omega + dOmegadt*dt
 position = position + velocity*dt
 velocity = velocity + F_act * dt / (m_ball + np.sum(dot_masses))
 
-if np.dot(velocity, absolute_system[3,:]) < 0.0 and position[2] <= ball_radious + eps:
+if np.dot(velocity, absolute_system[3,:]) < 0.0 and position[2] <= ball_radious + eps: # Enforce correction of the velocity
     velocity = velocity - absolute_system[3,:]*np.dot(absolute_system[3,:], velocity)
+
+Omega_Rot = omega_matrix(Omega)
+U = np.dot(U, Omega_Rot)
+A = np.dot(U, np.diag([np.sqrt(1.0/8.0)]*4))
+
+w_abs  = w_abs + e_abs * dt
+W = np.dot(U, np.diag(w_abs))
+E = np.dot(U, np.diag(e_abs))
+e_abs = controller() # input from controller
+
+
+
+
+
