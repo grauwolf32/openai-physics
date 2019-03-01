8f2b2d1fd40197e1b80885ccd37fe3b5eeecc5cd
grauwolf32 <grauwolf32@mail.ru>
2019-03-01 21:41:03 +0300

    benchmark

---
+++ benckmark.py
@@ -0,0 +1,16 @@
+import time
+import physics
+
+def main():
+    hs = physics.HyroSphere(t_len=1.0, mass=8, dot_masses=[1.0]*4, position=[0.0,0.0,0.0])
+    n = 10000
+    dt = 0.01
+    t1 = time.time()
+    for i in range(0, n):
+        hs.move(dt, [0.0]*4)
+    t2 = time.time()
+
+    print("{} trials took {} seconds".format(n, t2-t1))
+
+if __name__ == "__main__":
+    main()
